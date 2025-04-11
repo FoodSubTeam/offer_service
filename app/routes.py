2a0cf1bd0bcbb50ca30fb24e280a6261ed6e0518
agata <agata.piasecka@outlook.com>
2025-04-11 16:39:13 +0200

    fix service for post forwarding and routing

--- app/routes.py
+++ app/routes.py
@@ -13,12 +13,12 @@ async def get_db():
     async with SessionLocal() as session:
         yield session
 
-@router.post("/create-offer/")
+@router.post("/offer/create-offer/")
 async def create_offer(offer: OfferCreate, db: AsyncSession = Depends(get_db)):
     await offerService.createOffer(offer, db)
-    return {"message": "User created successfully"}
+    return {"message": "Offer created successfully!"}
 
-@router.get("/offers/")
+@router.get("/offer/offers/")
 async def get_all_offers(db: AsyncSession = Depends(get_db)):
     offers = await offerService.get_all_offers(db)
     return {"offers": offers}
